6ea3b2e0c67442ca6388e285a0962876d783be42
jaros <judin.jaroslav@gmail.com>
2018-03-15 11:00:37 +0100

    add confirmation for saving

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
-import {LoadingController} from 'ionic-angular';
-import {AlertController} from 'ionic-angular';
+import {Loading, LoadingController, AlertController, ToastController} from 'ionic-angular';
 import {Validators, FormBuilder, FormGroup} from '@angular/forms';
 import {HTTP} from '@ionic-native/http';
 
@@ -12,7 +11,9 @@ export class HomePage {
 
   private contact: FormGroup;
 
-  constructor(private loadingCtrl: LoadingController, private http: HTTP,
+  private loading;
+
+  constructor(private loadingCtrl: LoadingController, private http: HTTP, private toastCtrl: ToastController,
               private formBuilder: FormBuilder, private alertCtrl: AlertController) {
     this.contact = this.formBuilder.group({
       email: ['', Validators.email]
@@ -22,14 +23,20 @@ export class HomePage {
   saveEmail() {
     console.log('saved', this.contact.value);
     if (this.contact.valid) {
-      this.presentLoading(`Saving... ${this.contact.value.email}`);
+      this.loading = this.presentLoading(`Saving... ${this.contact.value.email}`);
       this.http.get('https://script.google.com/macros/s/AKfycbxXLZYReQ0s4ZzVSOkHbKci3ITEcnhMnuv4BwfzfiJH2MpkoN4/exec',
         {"email_field": this.contact.value.email}, {})
         .then(data => {
           console.log(data.status);
           console.log(data.data); // data received by server
           console.log(data.headers);
-
+          this.loading.dismissAll();
+          this.toastCtrl.create({
+            message: `${this.contact.value.email} is successfully subscribed`,
+            duration: 3000,
+            position: 'middle'
+          }).present();
+          this.contact.reset();
         })
         .catch(error => {
           console.log(error);
@@ -48,11 +55,13 @@ export class HomePage {
     alert.present();
   }
 
-  presentLoading(message: string, duration: number = 2000) {
-    this.loadingCtrl.create({
+  presentLoading(message: string): Loading {
+    let loading = this.loadingCtrl.create({
       content: message,
-      duration: 2000,
+      // duration: 2000,
       dismissOnPageChange: true
-    }).present();
+    });
+    loading.present();
+    return loading;
   }
 }
